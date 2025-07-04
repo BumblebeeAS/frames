22d0dc34a6c60441f59071299656f1eb08e66855
monkescripts <shaolianghe0.0@gmail.com>
2025-07-04 23:28:04 +0800

    Edit launch.py

--- launch/launch.py
+++ launch/launch.py
@@ -7,15 +7,15 @@ from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     ld = LaunchDescription()
-    ld.add_action(DeclareLaunchArgument("controls_frame", default_value="world_ned"))
+    ld.add_action(DeclareLaunchArgument("controls_frame", default_value="map_ned"))
     ld.add_action(
-        DeclareLaunchArgument("base_frame", default_value="auv4/base_link_ned")
+        DeclareLaunchArgument("base_frame", default_value="orca4_ned")
     )
 
     ld.add_action(
         DeclareLaunchArgument(
             "namespace",
-            default_value="auv4",
+            default_value="mini",
             description="Namespace of the vehicle",
         )
     )

fbaf206076a51e185a3a375db3aaa355777b7547
SamuelFoo <fooenzesamuel@gmail.com>
2025-12-20 20:21:31 +0800

    Add uav2 launch

---
+++ launch/uav2.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node, PushRosNamespace
+
+
+def generate_launch_description():
+    launch_objects = [PushRosNamespace("/uav2")]
+
+    nodes = [
+        Node(
+            package="frames",
+            executable="convert_to_controls_pose.py",
+            name="convert_to_controls_pose",
+            parameters=[
+                {
+                    "controls_frame": "odom_ned",
+                    "base_frame": "uav2/base_link_frd",
+                }
+            ],
+            output="screen",
+        )
+    ]
+
+    return LaunchDescription(launch_objects + nodes)
